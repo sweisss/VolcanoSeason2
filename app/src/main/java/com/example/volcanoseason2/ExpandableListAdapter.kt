b1028a74c478f34f4fcccc9f208f85595b8639f2
sweisss <sweisss@gmail.com>
2022-07-13 21:42:27 -0700

    Changed EquipmentChecklistActivity to ExpandableListView. Got skeleton file for ExpandableListAdapter.

---
+++ app/src/main/java/com/example/volcanoseason2/ExpandableListAdapter.kt
@@ -0,0 +1,47 @@
+package com.example.volcanoseason2
+
+import android.view.View
+import android.view.ViewGroup
+import android.widget.BaseExpandableListAdapter
+
+class ExpandableListAdapter : BaseExpandableListAdapter {
+    override fun getGroupCount(): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChildrenCount(p0: Int): Int {
+        TODO("Not yet implemented")
+    }
+
+    override fun getGroup(p0: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChild(p0: Int, p1: Int): Any {
+        TODO("Not yet implemented")
+    }
+
+    override fun getGroupId(p0: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChildId(p0: Int, p1: Int): Long {
+        TODO("Not yet implemented")
+    }
+
+    override fun hasStableIds(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    override fun getGroupView(p0: Int, p1: Boolean, p2: View?, p3: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun getChildView(p0: Int, p1: Int, p2: Boolean, p3: View?, p4: ViewGroup?): View {
+        TODO("Not yet implemented")
+    }
+
+    override fun isChildSelectable(p0: Int, p1: Int): Boolean {
+        TODO("Not yet implemented")
+    }
+}
