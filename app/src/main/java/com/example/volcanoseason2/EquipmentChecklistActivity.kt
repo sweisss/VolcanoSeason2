58c387be9065f2859cc5a73b367bc22dabae745c
sweisss <sweisss@gmail.com>
2022-07-13 22:01:26 -0700

    Ready to populate ArrayLists.

--- app/src/main/java/com/example/volcanoseason2/EquipmentChecklistActivity.kt
+++ app/src/main/java/com/example/volcanoseason2/EquipmentChecklistActivity.kt
@@ -6,10 +6,12 @@ import android.os.Bundle
 class EquipmentChecklistActivity : AppCompatActivity() {
 
     val header : MutableList<String> = ArrayList()
-    val body : MutableList<String> = ArrayList()
+    val body : MutableList<MutableList<String>> = ArrayList()
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_equipment_checklist)
+
+        // populate ArrayLists here (busy work!)
     }
 }
