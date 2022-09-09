39cbecc390c6a90c416652983b713ad61bccde64
sweisss <sweisss@gmail.com>
2022-09-08 21:33:39 -0700

    created an empty activity for the html checklist.

---
+++ app/src/main/java/com/example/volcanoseason2/EquipmentChecklistHTMLActivity.kt
@@ -0,0 +1,11 @@
+package com.example.volcanoseason2
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class EquipmentChecklistHTMLActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_equipment_checklist_htmlactivity2)
+    }
+}
